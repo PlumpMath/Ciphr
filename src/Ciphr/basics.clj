04bc31dffd0d7e18151713cc150f3bbd55808dc7
Erik Svedäng <erik.svedang@gmail.com>
2012-08-17 08:11:05 +0200

    Basic cipher is working

--- src/Ciphr/basics.clj
+++ src/Ciphr/basics.clj
@@ -1,4 +1,19 @@
-(ns Ciphr.basics)
+(ns Ciphr.basics
+	(:require clojure.string))
+
+(def substitution-schema {
+	\a \b
+	\b \c
+	\c \d
+	\d \e
+	\e \a
+	})
+
+(defn get-char [c]
+	(let [item (find substitution-schema c)]
+		(if (nil? item)
+			"_"
+			(val item))))
 
 (defn substitution [text]
-	text)
+	(clojure.string/join (map get-char text)))
