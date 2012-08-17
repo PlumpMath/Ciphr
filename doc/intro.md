d00257c47b1dc5a7fce8ecdc5807904ebb709630
Erik Svedäng <erik.svedang@gmail.com>
2012-08-17 07:41:35 +0200

    Project started

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to Ciphr
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/Ciphr/core_test.clj
@@ -0,0 +1,7 @@
+(ns Ciphr.core-test
+  (:use clojure.test
+        Ciphr.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
